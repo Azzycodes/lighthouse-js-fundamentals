b64aff0032d93bcd1f63d108d7400cd41dd1de96
Azzam <azzamathar@hotmail.com>
2019-06-17 02:26:35 +0000

    Declearing and calling functions

---
+++ hello.js
@@ -0,0 +1,12 @@
+const sayGreeting  = function () {
+  console.log("Greetings, Earthlings");
+}
+
+sayGreeting();
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
